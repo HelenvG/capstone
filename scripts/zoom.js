f8f1670d6c0482bc190b36259e7ce723dba504cd
HelenvG <helenvangils@live.nl>
2018-02-25 14:13:57 +0100

    test zoom function

---
+++ scripts/zoom.js
@@ -0,0 +1,16 @@
+var modal = document.getElementById('imgZoom');
+
+var img = document.getElementById("amoyImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() { 
+  modal.style.display = "none";
+}
