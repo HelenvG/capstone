64c51331fc5ce0e14f4c844959b53db2fcd22fa8
HelenvG <helenvangils@live.nl>
2018-02-27 11:18:14 +0100

    zoom jquery

--- scripts/zoom.js
+++ scripts/zoom.js
@@ -1,16 +1,45 @@
-var modal = document.getElementById('imgZoom');
+jQuery(document).ready(function($) {
+	
+	$('.lightbox_trigger').click(function(e) {
+		
+	
+		e.preventDefault();
+		
+		//Get image
+		var image_src = $(this).attr("src");
+		
 
-var img = document.getElementById("amoyImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
+		
+		if ($('#lightbox').length > 0) { // #lightbox exists
+			
+			//place href as img src value
+			$('#content').html('<img src="' + image_src + '" />');
+		   	
+			//show lightbox window - you could use .show('fast') for a transition
+			$('#lightbox').show();
+		}
+		
+		else { //#lightbox does not exist - create and insert (runs 1st time only)
+			
+			//create HTML markup for lightbox window
+			var lightbox = 
+			'<div id="lightbox">' +
+				'<p>&times</p>' +
+				'<div id="content">' + //insert clicked link's href into img src
+					'<img src="' + image_src +'" />' +
+				'</div>' +	
+			'</div>';
+				
+			//insert lightbox HTML into page
+			$('body').append(lightbox);
+		}
+		
+	});
+	
+	//Click to close lightbox
+	$('#lightbox').live('click', function() { 
+		$('#lightbox').hide();
+	});
 
-var span = document.getElementsByClassName("close")[0];
+});
 
-span.onclick = function() { 
-  modal.style.display = "none";
-}
