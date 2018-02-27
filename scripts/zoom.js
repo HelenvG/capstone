ad25e4dc0ad87e37ba476abb5a04603e93ff3349
HelenvG <helenvangils@live.nl>
2018-02-27 11:51:24 +0100

    zoom adjustment

--- scripts/zoom.js
+++ scripts/zoom.js
@@ -2,41 +2,33 @@ jQuery(document).ready(function($) {
 	
 	$('.lightbox_trigger').click(function(e) {
 		
-	
 		e.preventDefault();
 		
-		//Get image
 		var image_src = $(this).attr("src");
 		
-
-		
-		if ($('#lightbox').length > 0) { // #lightbox exists
+		if ($('#lightbox').length > 0) { 
 			
-			//place href as img src value
 			$('#content').html('<img src="' + image_src + '" />');
 		   	
-			//show lightbox window - you could use .show('fast') for a transition
 			$('#lightbox').show();
 		}
 		
-		else { //#lightbox does not exist - create and insert (runs 1st time only)
+		else { 
 			
-			//create HTML markup for lightbox window
 			var lightbox = 
 			'<div id="lightbox">' +
 				'<p>&times</p>' +
-				'<div id="content">' + //insert clicked link's href into img src
+				'<div id="content">' + 
 					'<img src="' + image_src +'" />' +
 				'</div>' +	
 			'</div>';
 				
-			//insert lightbox HTML into page
 			$('body').append(lightbox);
 		}
 		
 	});
 	
-	//Click to close lightbox
+
 	$('#lightbox').live('click', function() { 
 		$('#lightbox').hide();
 	});
