053467ed0be00c9b4757a8e527bebca5adc85bdc
HelenvG <helenvangils@live.nl>
2018-03-04 16:25:09 +0100

    clean up code

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,24 +1,20 @@
 $(document).ready(function() {
 	$('ul#filter a').click(function() {
-		$(this).css('outline','none');
+		$(this).css('outline', 'none');
 		$('ul#filter .current').removeClass('current');
 		$(this).parent().addClass('current');
-		
-		var filterVal = $(this).text().toLowerCase().replace(' ','-');
-				
-		if(filterVal == 'all') {
-			$('ul#filter_wrapper li.hidden').fadeIn('slow').removeClass('hidden');
+		var filterVal = $(this).text().toLowerCase().replace(' ', '-');
+		if (filterVal == 'all') {
+			$('#filter_wrapper li.hidden').fadeIn('slow').removeClass('hidden');
 		} else {
-			
-			$('ul#filter_wrapper li').each(function() {
-				if(!$(this).hasClass(filterVal)) {
+			$('#filter_wrapper li').each(function() {
+				if (!$(this).hasClass(filterVal)) {
 					$(this).fadeOut('normal').addClass('hidden');
 				} else {
 					$(this).fadeIn('slow').removeClass('hidden');
 				}
 			});
 		}
-		
 		return false;
 	});
 });
